9085c5e7c91e210f5f375e4cb807a83a599b0894
Filip Masny <Filip@MacBook-Pro.local>
2025-10-27 19:47:44 +0100

    Dodano target=_self do linków menu

--- portfolio-tracker/ui_common.py
+++ portfolio-tracker/ui_common.py
@@ -48,9 +48,9 @@ def render_navigation_menu():
     </style>
     
     <div class="nav-menu">
-        <a href="streamlit_app.py" class="nav-link">Główna</a>
-        <a href="pages/1_kryptowaluty.py" class="nav-link">Kryptowaluty</a>
-        <a href="pages/2_akcje.py" class="nav-link">Akcje</a>
+        <a href="streamlit_app.py" class="nav-link" target="_self">Główna</a>
+        <a href="pages/1_kryptowaluty.py" class="nav-link" target="_self">Kryptowaluty</a>
+        <a href="pages/2_akcje.py" class="nav-link" target="_self">Akcje</a>
     </div>
     """, unsafe_allow_html=True)
 
